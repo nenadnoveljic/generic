a5f86fea5eb69a76b8be20fda0db7f6dcaae85d2
nenadnoveljic <nenad.noveljic@gmail.com>
2018-06-22 11:40:11 +0000

    Update CheckConnectivity.pl

--- CheckConnectivity.pl
+++ CheckConnectivity.pl
@@ -1,75 +1,25 @@
-#!/usr/bin/perl -w
+/usr/bin/perl -w                                                                                                                                                                                                 
 
-# Author: Nenad Noveljic
-  
-use strict ;
 
-use IO::Socket::INET;
 
-my ( $server , $port ) = @ARGV ;
-print "INFO: Checking connection to $server:$port\n" ;
 
-# auto-flush on socket
-$| = 1;
 
-TRY:
-while (1) {
-  sleep 1 ;
-  my $begin = time ;
-  
-  my $socket = new IO::Socket::INET (
-      PeerHost => $server ,
-      PeerPort => $port ,
-      Proto => 'tcp',
-  );
-  
-  if ( !$socket ) {
-    print localtime . ' ' ;
-    print "ERROR:" . 
-      "Couldn't open socket to $server:$port : $!\n" ;
-oracle@svdbp01i:/export/home/oracle/workspace_vbnov/check_connectiivty_reselience/libperl/
-DFEX10 $ more CheckConnectivity.pl                                                                                                                                                                                 
-#!/usr/bin/perl -w                                                                                                                                                                                                 
-                                                                                                                                                                                                                   
-# Author: Nenad Noveljic                                                                                                                                                                                           
-                                                                                                                                                                                                                   
-use strict ;                                                                                                                                                                                                       
-                                                                                                                                                                                                                   
-use IO::Socket::INET;                                                                                                                                                                                              
-                                                                                                                                                                                                                   
-my ( $server , $port ) = @ARGV ;                                                                                                                                                                                   
-print "INFO: Checking connection to $server:$port\n" ;                                                                                                                                                             
-                                                                                                                                                                                                                   
-# auto-flush on socket                                                                                                                                                                                             
-$| = 1;                                                                                                                                                                                                            
-                                                                                                                                                                                                                   
-TRY:                                                                                                                                                                                                               
-while (1) {                                                                                                                                                                                                        
-  sleep 1 ;                                                                                                                                                                                                        
-  my $begin = time ;                                                                                                                                                                                               
-                                                                                                                                                                                                                   
-  my $socket = new IO::Socket::INET (                                                                                                                                                                              
-      PeerHost => $server ,                                                                                                                                                                                        
-      PeerPort => $port ,                                                                                                                                                                                          
-      Proto => 'tcp',                                                                                                                                                                                              
-  );                                                                                                                                                                                                               
-                                                                                                                                                                                                                   
-  if ( !$socket ) {                                                                                                                                                                                                
-    print localtime . ' ' ;                                                                                                                                                                                        
-    print "ERROR:" .                                                                                                                                                                                               
-      "Couldn't open socket to $server:$port : $!\n" ;                                                                                                                                                             
-    next TRY ;                                                                                                                                                                                                     
-  }                                                                                                                                                                                                                
-                                                                                                                                                                                                                   
-  $socket->close() ;                                                                                                                                                                                               
-                                                                                                                                                                                                                   
-  my $elapsed = time - $begin ;
-  
-  if ( $elapsed > 1 ) {
-    print localtime . ' ' ;
-    print "WARNING: " . 
-      "TCP socket open und close took $elapsed seconds in total\n" 
-    ;
-  }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
